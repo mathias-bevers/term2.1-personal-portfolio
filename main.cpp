ed459b24e48de4fd90ce83f1a0484691c384e3bb
Mathias <mathiasbevers@outlook.com>
2023-09-14 13:39:40 +0200

    configured vs-code to run and debug makefiles

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include "src/calculator.hpp"
 #include <iostream>
+#include <limits>
+
+#include "src/calculator.hpp"
 
 int main() {
     std::cout << "Mathias Bevers personal portfolio 1\n\n";
 
     personal_portfolio::Calculator calculator;
     calculator.run();
+
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,5 @@
+# To intergrade with vs-code look at https://hackernoon.com/how-to-set-up-c-debugging-in-vscode-using-a-makefile
+
 CC = g++
 CFLAGS = -g -Wall
 
